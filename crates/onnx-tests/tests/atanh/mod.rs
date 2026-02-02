b47827648f0a7bc53dbad7f3d71d1bc7b46aeb60
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2026-02-02 09:40:12 -0600

    Fix formatting

--- crates/onnx-tests/tests/atanh/mod.rs
+++ crates/onnx-tests/tests/atanh/mod.rs
@@ -14,8 +14,7 @@ mod tests {
         let device = Default::default();
         let model: atanh::Model<TestBackend> = atanh::Model::new(&device);
 
-        let input =
-            Tensor::<TestBackend, 4>::from_floats([[[[-0.5, 0.0, 0.5, 0.9]]]], &device);
+        let input = Tensor::<TestBackend, 4>::from_floats([[[[-0.5, 0.0, 0.5, 0.9]]]], &device);
 
         let output = model.forward(input);
         let expected = TensorData::from([[[[-0.5493f32, 0.0000, 0.5493, 1.4722]]]]);
