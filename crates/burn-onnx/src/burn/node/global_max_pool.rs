6ed4f76183d16e7f8880d242ae9ae1cbc282ee87
Aditya Khedekar <65857172+aditya0by0@users.noreply.github.com>
2026-02-21 11:37:56 +0000

    use max dims

--- crates/burn-onnx/src/burn/node/global_max_pool.rs
+++ crates/burn-onnx/src/burn/node/global_max_pool.rs
@@ -9,59 +9,29 @@ impl NodeCodegen for onnx_ir::node::global_max_pool::GlobalMaxPoolNode {
         &self.outputs
     }
 
-    fn field(&self) -> Option<Field> {
-        // Determine field type based on input dimension
-        let input = self.inputs.first().unwrap();
-        let rank = match &input.ty {
+    fn forward(&self, scope: &mut ScopeAtPosition<'_>) -> TokenStream {
+        let input = scope.arg(self.inputs.first().unwrap());
+        let output = arg_to_ident(self.outputs.first().unwrap());
+
+        let input_arg = self.inputs.first().unwrap();
+        let rank = match &input_arg.ty {
             ArgType::Tensor(t) => t.rank,
             _ => panic!("Expected tensor input for GlobalMaxPool"),
         };
 
-        let name = Ident::new(&self.name, Span::call_site());
-
-        let (field_type, init_tokens) = match rank {
-            3 => (
-                quote! { MaxPool1d },
-                quote! {
-                    let #name = MaxPool1dConfig::new(1)
-                        .init();
-                },
-            ),
-            4 => (
-                quote! { MaxPool2d },
-                quote! {
-                    let #name = MaxPool2dConfig::new([1, 1])
-                        .init();
-                },
-            ),
-            dim => panic!("Unsupported input dim ({dim}) for GlobalMaxPoolNode"),
-        };
-
-        Some(Field::new(self.name.clone(), field_type, init_tokens))
-    }
-
-    fn forward(&self, scope: &mut ScopeAtPosition<'_>) -> TokenStream {
-        let input = scope.arg(self.inputs.first().unwrap());
-        let output = arg_to_ident(self.outputs.first().unwrap());
-        let field = Ident::new(&self.name, Span::call_site());
-
-        quote! {
-            let #output = self.#field.forward(#input);
-        }
-    }
-
-    fn register_imports(&self, imports: &mut BurnImports) {
-        let input = self.inputs.first().unwrap();
-        let rank = input.ty.rank();
-
+        // GlobalMaxPool reduces over all spatial dimensions, keeping batch and channel dims
+        // 3D: [B, C, H] -> reduce dims 1, 2 -> [B, C, 1]
+        // 4D: [B, C, H, W] -> reduce dims 2, 3 -> [B, C, 1, 1]
         match rank {
             3 => {
-                imports.register("burn::nn::pool::MaxPool1d");
-                imports.register("burn::nn::pool::MaxPool1dConfig");
+                quote! {
+                    let #output = #input.max_dim(2usize);
+                }
             }
             4 => {
-                imports.register("burn::nn::pool::MaxPool2d");
-                imports.register("burn::nn::pool::MaxPool2dConfig");
+                quote! {
+                    let #output = #input.max_dim(3usize).max_dim(2usize);
+                }
             }
             dim => panic!("Unsupported input dim ({dim}) for GlobalMaxPoolNode"),
         }
@@ -95,7 +65,7 @@ mod tests {
         let code = codegen_forward_default(&node);
         assert_snapshot!(code, @r"
         pub fn forward(&self, input: Tensor<B, 3>) -> Tensor<B, 3> {
-            let output = self.pool1.forward(input);
+            let output = input.max_dim(2usize);
             output
         }
         ");
@@ -107,7 +77,7 @@ mod tests {
         let code = codegen_forward_default(&node);
         assert_snapshot!(code, @r"
         pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
-            let output = self.pool1.forward(input);
+            let output = input.max_dim(3usize).max_dim(2usize);
             output
         }
         ");
@@ -119,7 +89,7 @@ mod tests {
         let code = codegen_forward_with_clone(&node);
         assert_snapshot!(code, @r"
         pub fn forward(&self, input: Tensor<B, 3>) -> Tensor<B, 3> {
-            let output = self.pool1.forward(input.clone());
+            let output = input.clone().max_dim(2usize);
             output
         }
         ");
@@ -131,7 +101,7 @@ mod tests {
         let code = codegen_forward_with_clone(&node);
         assert_snapshot!(code, @r"
         pub fn forward(&self, input: Tensor<B, 4>) -> Tensor<B, 4> {
-            let output = self.pool1.forward(input.clone());
+            let output = input.clone().max_dim(3usize).max_dim(2usize);
             output
         }
         ");
